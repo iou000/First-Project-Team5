3dcb7db1cd99f6fe066a939496146bba149c4dbf
iou000 <iou000@naver.com>
2022-03-09 23:48:36 +0900

    RecipeDAO 유저별 레시피 조회 메소드 생성

--- src/com/team5/dao/RecipeDAO.java
+++ src/com/team5/dao/RecipeDAO.java
@@ -10,12 +10,16 @@ import com.team5.vo.RecipeVO;
 
 import util.DBManager;
 
+
+
 /**
- * @author seop
- *
+ * @author    : seop
+ * @Date      : 2022. 3. 9.
+ * @ClassName : RecipeDAO
+ * @Comment   : 레시피 DAO
  */
 public class RecipeDAO {
-	private RecipeDAO() { } //싱글턴
+	private RecipeDAO() { } //싱글턴 처리
 	private static RecipeDAO instance = new RecipeDAO();
 	public static RecipeDAO getInstance() {
 		return instance;
@@ -25,9 +29,16 @@ public class RecipeDAO {
     CallableStatement cstmt = null;
 	ResultSet rs = null;
     
-	// 레시피 생성
+
+	/**
+	 * @Author  : seop
+	 * @Date    : 2022. 3. 9.
+	 * @Method  : insertRecipe
+	 * @return  : void
+	 * @Comment : 레시피 생성
+	 */
 	public void insertRecipe(RecipeVO recipeVO) {
-		//호출할 SQL 문장
+		// 호출할 SQL 문장
 		String runSP = "{ CALL recipes_pack.recipes_insert(?, ?, ?, ?, ?, ?, ?)}";
 		
 		try {
@@ -45,6 +56,7 @@ public class RecipeDAO {
 			cstmt.setInt(7, recipeVO.getUser_id());
 			
 			System.out.println(runSP);
+			//실행
 			cstmt.executeUpdate();
 			System.out.println("레시피 생성 완료");
 			
@@ -55,9 +67,16 @@ public class RecipeDAO {
 		}
 	}//end insertRecipe
 	
-	// 레시피 수정
+	
+	/**
+	 * @Author  : seop
+	 * @Date    : 2022. 3. 9.
+	 * @Method  : updateRecipe
+	 * @return  : void
+	 * @Comment : 레시피 수정
+	 */
 	public void updateRecipe(RecipeVO recipeVO) {
-		//호출할 저장 프로시저
+		// 호출할 저장 프로시저
 		String runSP = "{ CALL recipes_pack.recipes_update(?, ?, ?, ?, ?, ?, ?)}";
 		
 		try {
@@ -75,6 +94,7 @@ public class RecipeDAO {
 			cstmt.setString(7, recipeVO.getImage());
 			
 			System.out.println(runSP);
+			//실행
 			cstmt.executeUpdate();
 			System.out.println(recipeVO.getId()+"번  "+"레시피 수정 완료");
 			
@@ -85,9 +105,16 @@ public class RecipeDAO {
 		}
 	}//end updateRecipe
 	
-	// 레시피 삭제
+	
+	/**
+	 * @Author  : seop
+	 * @Date    : 2022. 3. 9.
+	 * @Method  : deleteRecipe
+	 * @return  : void
+	 * @Comment : 레시피 삭제
+	 */
 	public void deleteRecipe(int id) {
-		//호출할 저장 프로시저
+		// 호출할 저장 프로시저
 		String runSP = "{ CALL recipes_pack.recipes_delete(?)}";
 		
 		try {
@@ -99,6 +126,7 @@ public class RecipeDAO {
 			cstmt.setInt(1, id);
 			
 			System.out.println(runSP);
+			//실행
 			cstmt.executeUpdate();
 			System.out.println(id+"번 "+"레시피 삭제 완료");
 			
@@ -109,7 +137,14 @@ public class RecipeDAO {
 		}
 	}// end deleteRecipe
 	
-	// 레시피 상세 조회
+	
+	/**
+	 * @Author  : seop
+	 * @Date    : 2022. 3. 9.
+	 * @Method  : selectRecipeById
+	 * @return  : RecipeVO
+	 * @Comment : 레시피 상세 조회
+	 */
 	public RecipeVO selectRecipeById(int id) {
 		// 호출할 저장 프로시저
 		String runSP = "{ CALL recipes_pack.recipes_select_by_id(?, ?)}";
@@ -124,7 +159,7 @@ public class RecipeDAO {
 			cstmt.setInt(1, id);
 			// 출력 파라미터
 			cstmt.registerOutParameter(2, oracle.jdbc.OracleTypes.CURSOR);
-			//실행 (리턴값이 ResultSet임)
+			//실행 (리턴값: ResultSet)
 			cstmt.execute();
 			//레시피 상세 조회 결과 받아오기
 			rs = (ResultSet)cstmt.getObject(2);
@@ -152,11 +187,17 @@ public class RecipeDAO {
 		return recipeVO;
 	}//end selectRecipeById
 	
-	// 레시피 리스트 조회
+	
+	/**
+	 * @Author  : seop
+	 * @Date    : 2022. 3. 9.
+	 * @Method  : selectRecipeList
+	 * @return  : List<RecipeVO>
+	 * @Comment : 레시피 리스트 조회(카테고리,검색어, 평점순)
+	 */
 	public List<RecipeVO> selectRecipeList(String category, String search_text) {
-		//레시피 리스트 생성
+		// 레시피 리스트 생성
 		List<RecipeVO> recipeList= new ArrayList<>();
-		//레시피 VO 생성
 		// 호출할 저장 프로시저
 		String runSP = "{ CALL recipes_pack.recipes_select_list(?, ?, ?)}";
 		
@@ -170,7 +211,7 @@ public class RecipeDAO {
 			cstmt.setString(2, search_text);
 			// 출력 파라미터
 			cstmt.registerOutParameter(3, oracle.jdbc.OracleTypes.CURSOR);
-			//실행
+			//실행 (리턴값: ResultSet)
 			cstmt.execute();
 			//레시피 상세 조회 결과 받아오기
 			rs = (ResultSet)cstmt.getObject(3);
@@ -185,6 +226,7 @@ public class RecipeDAO {
 				
 				recipeList.add(recipeVO);
 			}
+			
 		} catch (Exception e){
 			e.printStackTrace();
 		} finally {
@@ -193,5 +235,52 @@ public class RecipeDAO {
 		
 		return recipeList;
 	}//end selectRecipeList
+	
+	/**
+	 * @Author  : seop
+	 * @Date    : 2022. 3. 9.
+	 * @Method  : selectRecipeListById
+	 * @return  : RecipeVO
+	 * @Comment : 각 유저별 레시피 리스트 조회
+	 */
+	public List<RecipeVO> selectRecipeListByUserId(int user_id) {
+		// 레시피 리스트 생성
+		List<RecipeVO> recipeList= new ArrayList<>();
+		// 호출할 저장 프로시저
+		String runSP = "{ CALL recipes_pack.recipes_select_list_by_user_id(?, ?)}";
+		
+		try {
+			// DB연결
+			conn = DBManager.getConnection();
+			// CallableStatement로 저장 프로시저 호출
+			cstmt = conn.prepareCall(runSP);
+			// 입력 파라미터
+			cstmt.setInt(1, user_id);
+			// 출력 파라미터
+			cstmt.registerOutParameter(2, oracle.jdbc.OracleTypes.CURSOR);
+			//실행 (리턴값: ResultSet)
+			cstmt.execute();
+			//레시피 상세 조회 결과 받아오기
+			rs = (ResultSet)cstmt.getObject(2);
+			while(rs.next()) {
+				RecipeVO recipeVO = new RecipeVO();
+				recipeVO.setId(rs.getInt("id"));
+				recipeVO.setTitle(rs.getString("title"));
+				recipeVO.setImage(rs.getString("image"));
+				recipeVO.setViewcount(rs.getInt("viewcount"));
+				recipeVO.setUsername(rs.getString("username"));
+				recipeVO.setGrade(rs.getInt("grade"));
+				
+				recipeList.add(recipeVO);
+			}
+			
+		} catch (Exception e){
+			e.printStackTrace();
+		} finally {
+			DBManager.close(conn, cstmt, rs);
+		}
+		
+		return recipeList;
+	}//end selectRecipeListByUserId
 	
 }
